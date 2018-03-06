8a2fad8976a312b3e8a930dc8cd12cf62cf9e319
yuruiming <yuruiming@hoolai.com>
2018-03-06 17:34:07 +0800

    项目完成

--- reader/pages/movies/movie-detail/movie-detail.js
+++ reader/pages/movies/movie-detail/movie-detail.js
@@ -1,61 +1,25 @@
-var util = require("../../../utils/util.js");
+import {
+  Movie
+} from 'class/Movie.js';
 var app = getApp();
 Page({
   data: {
     movie: {}
-  },
+  }, //es6,module,class,promise =>
   onLoad: function(options) {
     var movieId = options.id;
     var url = app.globalData.doubanBase +
       "/v2/movie/subject/" + movieId;
-    util.http(url, this.processDoubanData);
-    // var movie = new Movie(url);
+    // util.http(url, this.processDoubanData);
+    var movie = new Movie(url);
 
-    // movie.getMovieData((movie) => {
-    //   this.setData({
-    //     movie: movie
-    //   })
-    // })
-  },
-  //回调函数
-  processDoubanData: function(data) {
-    if (!data) {
-      return;
-    }
-    console.log(data) //学会看数据  重点处理数据为空值  豆瓣数据处理 视频9-6
-    var director = {
-      avatar: '',
-      name: '',
-      id: ''
-    }
-    if (data.directors[0] != null) {
-      if (data.directors[0].avatars != null) {
-        director.avatar = data.directors[0].avatars.large
-      }
-      director.name = data.directors[0].name;
-      director.id = data.directors[0].id;
-    }
-    var movie = {
-      movieImg: data.images ? data.images.large : '',
-      country: data.countries[0],
-      title: data.title,
-      originalTitle: data.original_title,
-      wishCount: data.wish_count,
-      commentCount: data.comments_count,
-      year: data.year,
-      generes: data.genres.join(","),
-      stars: util.convertToStarsArray(data.rating.stars),
-      score: data.rating.average,
-      director: director,
-      casts: util.convertToCastString(data.casts),
-      castsInfo: util.convertToCastInfos(data.casts),
-      summary: data.summary
-    }
-    this.setData({
-      movie: movie
+    movie.getMovieData((movie) => {
+      this.setData({
+        movie: movie
+      })
     })
-
   },
+
   viewMoviePostImg: function(e) {
     var src = e.currentTarget.dataset.src;
     wx.previewImage({
